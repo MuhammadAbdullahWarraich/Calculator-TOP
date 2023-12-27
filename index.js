f4490917f3a160a9a6fdd0eec2fe638e09a5bbfc
Muhammad Abdullah Warraich <muhammadabdullah2016pk@gmail.com>
2023-12-27 22:03:44 +0500

    feat: mvp

--- index.js
+++ index.js
@@ -8,12 +8,60 @@ class Operator{//class was used as it is more semantic
 const Operations = [
     new Operator("+", (a,b)=>Number(a)+Number(b), "binary"),
     new Operator("-", (a,b)=>Number(a)-Number(b), "binary"),
-    new Operator("*", (a,b)=>Number(a)*Number(b), "binary"),
+    new Operator("x", (a,b)=>Number(a)*Number(b), "binary"),
     new Operator("/", (a,b)=>Number(a)/Number(b), "binary"),
-    new Operator("-", (a)=>-1 * Number(a), "unary"),
 ];
-console.table(Operations);
-let a, b, operator;
+//console.table(Operations);
+let a=undefined, b=undefined, operator=undefined;
 function operate(op,x,y){
-    return Operations.filter(op=>op.symbol===op).operation(a,b);
+    return Operations.find(operation=>operation.symbol===op).operation(x,y);
 }
+const numbers = document.querySelectorAll(".digit");
+const operations = document.querySelectorAll(".operations");
+numbers.forEach(number=>number.addEventListener('click', (e)=>{
+    const screen = document.querySelector(".display-screen");
+    if (a===undefined){
+        a=Number(number.textContent);
+        screen.textContent+=a;
+    }
+    else {
+        b=Number(number.textContent);
+        screen.textContent+=b;        
+    }
+}));
+operations.forEach(op=>op.addEventListener("click", (e)=>{
+    const screen = document.querySelector(".display-screen");
+    const outputscreen = document.querySelector(".result-screen");
+    if (b!==undefined){
+        a=operate(operator, a, b);
+        b=undefined;
+        outputscreen.textContent=a;
+    }
+    if (b===0 && operator==="/"){
+        outputscreen.textContent="Division by zero error";
+        a=undefined;
+        b=undefined;
+    }
+    operator = e.target.textContent;
+    if (a!==undefined)screen.textContent += operator;
+}))
+document.querySelector("#solve").addEventListener("click", (e)=>{
+    const outputscreen = document.querySelector(".result-screen");
+    if (a!=undefined && b!=undefined){
+        a=operate(operator, a, b);
+        if (b===0 && operator==="/") outputscreen.textContent="Division by zero error";
+        b=undefined;
+        //clrscr();
+        outputscreen.textContent=a;
+    }
+})
+function clrscr(){
+    const screen = document.querySelector(".display-screen");
+    screen.textContent = "";
+    a=undefined;
+    b=undefined;
+    operator=undefined;
+    const outputscreen = document.querySelector(".result-screen");
+    outputscreen.textContent = "";
+}
+document.querySelector("#clrscr").addEventListener("click", clrscr);
